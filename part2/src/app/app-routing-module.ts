cba7dfd34b0341f156974e0883b3606b7fec2859
jike996633-del <jike996633@gmail.com>
2026-04-08 22:01:25 +0800

    part2: fix component class name mismatch, complete routing setup

--- part2/src/app/app-routing-module.ts
+++ part2/src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// 👇 类名+路径100%匹配你所有组件的真实导出
+import { Home } from './home/home';
+import { ItemAdd } from './item-add/item-add';
+import { ItemList } from './item-list/item-list';
+import { ItemDetail } from './item-detail/item-detail';
+
+const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'add', component: ItemAdd },
+  { path: 'list', component: ItemList },
+  { path: 'detail/:id', component: ItemDetail },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
