e94b68c3fa0cc3b763111507262a29898c6dab6b
jike996633-del <jike996633@gmail.com>
2026-04-08 22:45:45 +0800

    part2: fix all errors, complete module setup

--- part2/src/app/app-module.ts
+++ part2/src/app/app-module.ts
@@ -1,6 +1,8 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+// 👇 完全、彻底、100% 匹配你的文件结构
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './home/home';
@@ -9,9 +11,19 @@ import { ItemList } from './item-list/item-list';
 import { ItemDetail } from './item-detail/item-detail';
 
 @NgModule({
-  declarations: [App, Home, ItemAdd, ItemList, ItemDetail],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+  declarations: [
+    App,
+    Home,
+    ItemAdd,
+    ItemList,
+    ItemDetail
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
